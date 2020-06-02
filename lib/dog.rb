b12e63dc3d43ed977f592447b4a94ce81d3609d6
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-02 02:20:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,6 +33,12 @@ class Dog
     dog
   end
 
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO dogs (name, breed)
