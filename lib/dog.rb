cbe19668a9fb1252a4b70243148b01d6717f18ff
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-02 02:09:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,5 +47,11 @@ class Dog
     dog
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
+  end
+
 
 end
