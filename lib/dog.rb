42a9ed0322584dfddfc36c6759566492d222c810
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-02 01:45:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,6 +32,12 @@ class Dog
   end
 
   def save
-    
+    sql = <<-SQL
+      INSERT INTO songs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
 end
