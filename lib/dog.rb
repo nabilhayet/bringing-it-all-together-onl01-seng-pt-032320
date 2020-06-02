c7f9d263a9224c2e4c67c0a97b43b37dae73e5a6
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-02 02:18:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,7 +26,10 @@ class Dog
   end
 
   def self.new_from_db(row)
-    dog = Dog.new(row[0],row[1],row[2])
+    id  =   row[0]
+    name =  row[1]
+    breed = row[2]
+    dog = Dog.new(id:id, name:name, breed:breed)
     dog
   end
 
